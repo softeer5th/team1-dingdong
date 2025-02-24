16338cb6387805f31991e933a0a7679ffb253b9f
popopy0412 <philip000412@gmail.com>
2025-02-24 17:43:53 +0900

    [fix]: SimpleCache에 버스 스케쥴 ID와 사용자 ID를 단일 키로 저장
    두 속성 모두 Long 타입으로 겹칠 수 있기 때문에, 두 값 앞에 각각 busSchedule, user 태그를 붙여 String 키 값으로 변환하여 저장하도록 수정했습니다.

--- backend/src/main/java/com/ddbb/dingdong/domain/reservation/service/ReservationConcurrencyManager.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/reservation/service/ReservationConcurrencyManager.java
@@ -1,11 +1,12 @@
 package com.ddbb.dingdong.domain.reservation.service;
 
+import com.ddbb.dingdong.domain.reservation.service.error.ReservationErrors;
 import com.ddbb.dingdong.domain.transportation.entity.BusSchedule;
 import com.ddbb.dingdong.domain.transportation.repository.BusScheduleQueryRepository;
 import com.ddbb.dingdong.domain.transportation.repository.projection.BusScheduleIdAndReservedSeatsProjection;
 import com.ddbb.dingdong.domain.transportation.service.BusErrors;
 import com.ddbb.dingdong.infrastructure.cache.SimpleCache;
-import com.ddbb.dingdong.infrastructure.lock.ChannelLock;
+import com.ddbb.dingdong.infrastructure.lock.StoppableSemaphore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -14,9 +15,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
@@ -24,26 +23,26 @@ import java.util.concurrent.TimeUnit;
 public class ReservationConcurrencyManager {
     private final SimpleCache cache;
     private final BusScheduleQueryRepository busScheduleQueryRepository;
-    public enum Type { SEMAPHORE, LOCK };
     private static final int EXPIRATION_TIME_MINUTES = 5;
     private static final int EXPIRATION_TIME_DAYS = 2;
+    
+    private static final String CACHE_ID_BUS_SCHEDULE = "busSchedule";
+    private static final String CACHE_ID_USER = "user";
 
     @EventListener(ApplicationReadyEvent.class)
     protected void init() {
         List<BusScheduleIdAndReservedSeatsProjection> projections = busScheduleQueryRepository.queryReadyBusSchedules();
         for (BusScheduleIdAndReservedSeatsProjection projection : projections) {
-            cache.put(Map.entry(projection.getBusScheduleId(), Type.SEMAPHORE), new Semaphore(15 - projection.getReservedSeats(), true), Duration.ofDays(EXPIRATION_TIME_DAYS));
-            cache.put(Map.entry(projection.getBusScheduleId(), Type.LOCK), new ChannelLock(), Duration.ofDays(EXPIRATION_TIME_DAYS));
+            cache.put(String.format(CACHE_ID_BUS_SCHEDULE + ":%d", projection.getBusScheduleId()), new StoppableSemaphore(15 - projection.getReservedSeats(), true), Duration.ofDays(EXPIRATION_TIME_DAYS));
         }
     }
 
     public void initReservationData(BusSchedule busSchedule) {
-        cache.put(Map.entry(busSchedule.getId(), Type.SEMAPHORE), new Semaphore(busSchedule.getRemainingSeats(), true), Duration.ofDays(EXPIRATION_TIME_DAYS));
-        cache.put(Map.entry(busSchedule.getId(), Type.LOCK), new ChannelLock(), Duration.ofDays(EXPIRATION_TIME_DAYS));
+        cache.put(String.format(CACHE_ID_BUS_SCHEDULE + ":%d",busSchedule.getId()), new StoppableSemaphore(busSchedule.getRemainingSeats(), true), Duration.ofDays(EXPIRATION_TIME_DAYS));
     }
 
     public void addUserToTimeLimitCache(Long userId, Long busScheduleId) {
-        cache.put(userId, busScheduleId, () -> {
+        cache.put(String.format(CACHE_ID_USER + ":%d", userId), busScheduleId, () -> {
             releaseSemaphore(busScheduleId);
         }, Duration.ofMinutes(EXPIRATION_TIME_MINUTES));
     }
@@ -57,9 +56,11 @@ public class ReservationConcurrencyManager {
             removeUserWithReleaseSemaphore(userId);
         }
 
-        Semaphore semaphore = (Semaphore) cache.get(Map.entry(busScheduleId, Type.SEMAPHORE));
+        StoppableSemaphore semaphore = (StoppableSemaphore) cache.get(String.format(CACHE_ID_BUS_SCHEDULE + ":%d", busScheduleId));
         try {
-            if (!semaphore.tryAcquire(0, TimeUnit.SECONDS)) {
+            if (semaphore == null) {
+                throw ReservationErrors.EXPIRED_RESERVATION_DATE.toException();
+            } else if (!semaphore.tryAcquire()) {
                 throw new InterruptedException();
             }
             return true;
@@ -69,40 +70,42 @@ public class ReservationConcurrencyManager {
     }
 
     public void releaseSemaphore(Long busScheduleId) {
-        Semaphore semaphore = (Semaphore) cache.get(Map.entry(busScheduleId, Type.SEMAPHORE));
-        semaphore.release();
+        StoppableSemaphore semaphore = (StoppableSemaphore) cache.get(String.format(CACHE_ID_BUS_SCHEDULE + ":%d", busScheduleId));
+        if (semaphore != null) {
+            semaphore.release();
+        }
     }
 
     public Long getUserInCache(Long userId) {
-        Object value = cache.get(userId);
+        Object value = cache.get(String.format(CACHE_ID_USER + ":%d", userId));
         if (value != null) return (Long) value;
         return null;
     }
 
     public Long removeUserWithReleaseSemaphore(Long userId) {
-        Object value = cache.removeWithTask(userId);
+        Object value = cache.removeWithTask(String.format(CACHE_ID_USER + ":%d", userId));
         if (value != null) return (Long) value;
         return null;
     }
 
     public Long removeUser(Long userId) {
-        Object value = cache.remove(userId);
+        Object value = cache.remove(String.format(CACHE_ID_USER + ":%d", userId));
         if (value != null) return (Long) value;
         return null;
     }
 
-    public boolean lockBusSchedule(Long busScheduleId) {
-        ChannelLock lock = (ChannelLock) cache.get(Map.entry(busScheduleId, Type.LOCK));
-        return lock.entryLock();
+    public void lockBusSchedule(Long busScheduleId) {
+        StoppableSemaphore lock = (StoppableSemaphore) cache.get(String.format(CACHE_ID_BUS_SCHEDULE + ":%d", busScheduleId));
+        lock.stop();
     }
 
-    public void unlockBusSchedule(Long busScheduleId) {
-        ChannelLock lock = (ChannelLock) cache.get(Map.entry(busScheduleId, Type.LOCK));
-        lock.unlock();
+    public void removeSemaphore(Long busScheduleId) {
+        cache.remove(String.format(CACHE_ID_BUS_SCHEDULE + ":%d", busScheduleId));
     }
 
     public int getRemainingSeats(Long busScheduleId) {
-        Semaphore semaphore = (Semaphore) cache.get(Map.entry(busScheduleId, Type.SEMAPHORE));
+        Semaphore semaphore = (Semaphore) cache.get(String.format(CACHE_ID_BUS_SCHEDULE + ":%d", busScheduleId));
         return semaphore.availablePermits();
     }
+
 }

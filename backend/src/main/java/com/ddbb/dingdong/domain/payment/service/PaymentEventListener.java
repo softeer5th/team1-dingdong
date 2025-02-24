ab8d1059f27529ccb5d72740735f05280f66212d
keemjoonsung <kjs990114@naver.com>
2025-02-25 00:46:50 +0900

    [fix] 환불 이벤트 리스너에 phase = TransactionPhase.AFTER_COMMIT 추가

--- backend/src/main/java/com/ddbb/dingdong/domain/payment/service/PaymentEventListener.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/payment/service/PaymentEventListener.java
@@ -2,11 +2,12 @@ package com.ddbb.dingdong.domain.payment.service;
 
 import com.ddbb.dingdong.domain.reservation.service.event.AllocationFailedEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -15,7 +16,7 @@ public class PaymentEventListener {
 
     @Async
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void refundEventListener(AllocationFailedEvent event) {
         Long userId = event.getUserId();
         Long reservationId = event.getReservationId();

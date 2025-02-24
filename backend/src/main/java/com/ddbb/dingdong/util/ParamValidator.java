6f93507226e8d01dbe8bdf84eb9d3ffb01ef6afd
keemjoonsung <kjs990114@naver.com>
2025-02-24 14:14:19 +0900

    [fix] 패스워드 정규식에서 대문자 필수 조건 삭제

--- backend/src/main/java/com/ddbb/dingdong/util/ParamValidator.java
+++ backend/src/main/java/com/ddbb/dingdong/util/ParamValidator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class ParamValidator {
     private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
-    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\\-_\\[\\]+=])(?=\\S+$).{8,20}$";
+    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&*()\\-_\\[\\]+=])(?=\\S+$).{8,20}$";
     private static final Pattern PASSWORD_PATTERN = Pattern.compile(PASSWORD_REGEX);
 
     /**

15b17ae963102eb3b93e290557f59d1189afac74
ji-woong-song <thdwldnd7@naver.com>
2025-02-24 11:50:47 +0900

    [fix]: 컨테이너 대기 부분 삭제
    사용자가 주석을 해제하여 사용하도록 유도
    
    softeer5th#329

--- backend/.simulate/run_simulate.sh
+++ backend/.simulate/run_simulate.sh
@@ -3,7 +3,7 @@
 docker compose up -d --build
 
 ## Spring Boot 컨테이너가 정상적으로 실행될 때까지 대기
-./wait-for-it.sh localhost:8080 -t 90 --strict -- echo "Spring Boot is up!"
+#./wait-for-it.sh localhost:8080 -t 90 --strict -- echo "Spring Boot is up!"
 
 ## Gatling 부하 테스트 실행
 #./stress-loader/mvnw gatling:test

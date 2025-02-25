f73cc6ae6d75ec4dc38f4b671a34eb2c4333668e
keemjoonsung <kjs990114@naver.com>
2025-02-25 11:14:22 +0900

    [fix] AES SECRET을 환경변수로 분리

--- backend/src/main/java/com/ddbb/dingdong/infrastructure/auth/encrypt/utils/AESEncoder.java
+++ backend/src/main/java/com/ddbb/dingdong/infrastructure/auth/encrypt/utils/AESEncoder.java
@@ -1,5 +1,7 @@
 package com.ddbb.dingdong.infrastructure.auth.encrypt.utils;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
@@ -14,7 +16,14 @@ public class AESEncoder {
     private static final String AES_GCM = "AES/GCM/NoPadding";
     private static final int GCM_TAG_LENGTH = 128;
     private static final byte[] IV = new byte[12];
-    private static final byte[] SERVER_KEY = "16ByteServerKey!".getBytes(StandardCharsets.UTF_8);
+    private static byte[] SERVER_KEY;
+    @Value("${security.secret.aes}")
+    private String secret;
+
+    @PostConstruct
+    public void init() {
+        SERVER_KEY = secret.getBytes(StandardCharsets.UTF_8);
+    }
 
     public String encrypt(String data) throws Exception {
         Cipher cipher = Cipher.getInstance(AES_GCM);
